5ce29d8a01811e598d91652de9cc9e90d003eca0
James A. Smith <jasmith86@gmail.com>
2019-04-03 11:39:24 -0400

    added logging

--- src/wifi_monitor.py
+++ src/wifi_monitor.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 import os, subprocess, sys
 import time
-import io
+import io, logging
 
 
 class Airodumper:
@@ -9,6 +9,7 @@ class Airodumper:
     ap_list = {}
     client_list = {}
     proc = None
+    interfaces = []
 
     def __init__(self):
         self.get_interfaces()
@@ -19,14 +20,20 @@ class Airodumper:
         try:
             self.proc = subprocess.Popen(['airmon-ng'], bufsize=1, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             self.proc.wait()
+
+            self.interfaces = []
             for line in io.TextIOWrapper(self.proc.stdout):
                 tmp = line.rstrip().split("\t")
                 if "phy" in tmp[0]:
-                    print("temp: ", tmp)
+                    logging.info("Interface: " + repr(tmp))
+                    self.interfaces.append(tmp[1])
+            logging.info("Found interfaces: " + repr(self.interfaces))
         except FileNotFoundError:
+            logging.error("aircrack-ng suite not found in PATH")
             sys.exit("Error: either aircrack-ng suite is not installed or not in PATH. Install from "
                      "https://github.com/aircrack-ng/aircrack-ng")
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     dumper = Airodumper()
