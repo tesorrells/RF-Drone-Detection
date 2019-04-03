bb4ab7d52e457dabd2ea176b8c0981811e5867a0
James A. Smith <jasmith86@gmail.com>
2019-04-03 12:39:15 -0400

    started choose interface

--- src/wifi_monitor.py
+++ src/wifi_monitor.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python3
-import os, subprocess, sys
-import time
-import io, logging
+import io
+import logging
+import os
+import subprocess
+import sys
 
 
 class Airodumper:
@@ -10,28 +12,64 @@ class Airodumper:
     client_list = {}
     proc = None
     interfaces = []
+    monitor_interfaces = []
 
     def __init__(self):
-        self.get_interfaces()
+        if not os.geteuid() == 0:
+            logging.error("Must run as root")
+            sys.exit('This script must be run as root!')
+        self.choose_interface()
 
     def get_interfaces(self):
-        if not os.geteuid() == 0:
-            sys.exit('This script must be run as root!')
+        logging.debug("Getting interfaces...")
         try:
-            self.proc = subprocess.Popen(['airmon-ng'], bufsize=1, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            self.proc.wait()
-
+            mon_proc = subprocess.Popen(['airmon-ng'], bufsize=1, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            mon_proc.wait()
+            # Get wifi interfaces
             self.interfaces = []
-            for line in io.TextIOWrapper(self.proc.stdout):
+            self.monitor_interfaces = []
+            for line in io.TextIOWrapper(mon_proc.stdout):
                 tmp = line.rstrip().split("\t")
                 if "phy" in tmp[0]:
-                    logging.info("Interface: " + repr(tmp))
+                    logging.debug("Found interface " + tmp[1] + " on " + tmp[0])
                     self.interfaces.append(tmp[1])
+                    if "mon" in tmp[1]:  # is interface in monitor mode already?
+                        logging.debug("\tinterface " + tmp[1] + " already in monitor mode")
+                        self.monitor_interfaces.append(tmp[1])
             logging.info("Found interfaces: " + repr(self.interfaces))
+            if len(self.interfaces) < 1:
+                raise ValueError
         except FileNotFoundError:
             logging.error("aircrack-ng suite not found in PATH")
             sys.exit("Error: either aircrack-ng suite is not installed or not in PATH. Install from "
-                     "https://github.com/aircrack-ng/aircrack-ng")
+                     "https://github.com/aircrack-ng/aircrack-ng ... Aborting")
+        except ValueError:
+            logging.error("Didn't find any wifi interfaces with airmon-ng")
+            sys.exit("Didn't find any wifi interfaces with airmon-ng... Aborting")
+
+    def stop_monitor_mode(self):
+        logging.info("Stopping monitor mode for all interfaces...")
+        self.get_interfaces()
+        for iface in self.monitor_interfaces:
+            logging.info("Stopping monitor mode for " + iface)
+            stop_proc = subprocess.Popen(['airmon-ng', 'stop', iface], bufsize=1, stdout=subprocess.PIPE,
+                                         stderr=subprocess.STDOUT)
+            stop_proc.wait()
+        self.get_interfaces()
+        logging.info("Monitor mode stopped for all interfaces...")
+
+    def choose_interface(self):
+        self.stop_monitor_mode()
+        print("Select interface:")
+        for i, iface in enumerate(self.interfaces):
+            print("%s\t%s" % (i, iface))
+        choice = None
+        while choice not in range(0, len(self.interfaces)):
+            try:
+                choice = int(input("Enter your selection & hit [Enter]: "))
+            except ValueError:
+                pass
+        logging.info("chose: " + self.interfaces[choice])
 
 
 if __name__ == '__main__':
