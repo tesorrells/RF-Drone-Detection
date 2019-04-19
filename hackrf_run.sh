a6af78679d06459644b8ff5f5a71a8bdf242e7dc
James A. Smith <jasmith86@gmail.com>
2019-04-19 16:14:08 -0400

    get Pipfile and script to run hackrf_sweep

---
+++ hackrf_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SAMPLE_DURATION=60
+read -p "Enter filename: " FILENAME
+FILENAME="$FILENAME.csv"
+echo $FILENAME
+if [ ! -f $FILENAME ]; then
+	echo "Starting to log data"
+	timeout $SAMPLE_DURATION hackrf_sweep -f 2400:2500 -n 8192 -w 600000 -r $FILENAME
+else
+	echo "File Already exists!!"
+fi
